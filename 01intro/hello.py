fa69bb1aa288a07846daef2f8d3ee7fdb9aa55c0
CasianPantilimon <casianpantilimon98@gmail.com>
2024-07-03 17:28:57 +0300

    TEST - commit

---
+++ 01intro/hello.py
@@ -0,0 +1,11 @@
+def multiply_by_two(num):
+    return num * 2
+
+
+def suma(n1, n2):
+    return n1 + n2
+
+
+print(multiply_by_two(2))
+
+print("hellooooo")
---
+++ 02.DataStructure/Bool_Int_Float.py
@@ -0,0 +1,25 @@
+"""
+Bool
+
+- sunt valorile True and False
+"""
+
+
+'''
+int &  float
+- int = numere intregi, ex: (2)
+- float = numere zecimale, ex: (2,5678)
+'''
+
+# print(2-4) se va afisa: - 2
+# print(2+4) se va afisa: 6
+# print(2*4) se va afisa: 8
+# print(2/4)  0.5
+
+# print(type(2-4)) se va afisa: class 'int'
+# print(type(2+4)) se va afisa: class 'int'
+# print(type(2*4)) se va afisa: class 'int'
+# print(type(2/4)) se va afisa: class 'float'
+
+
+

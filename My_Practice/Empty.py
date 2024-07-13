da8882b2b668f6477ca2b2044139ed2254b1b37d
CasianPantilimon <casianpantilimon98@gmail.com>
2024-07-14 00:13:41 +0300

    TEMA - Validator CNP

---
+++ My_Practice/Empty.py
@@ -0,0 +1,23 @@
+# CNP = "198052627169"
+#
+# suma_CNP = 0
+#
+# for i in CNP:
+#     suma_CNP += int(i)
+#
+# print(suma_CNP)
+#
+# for i in CNP:
+#
+#     if CNP[0] == "1":
+#         genul = "sex masculin"
+#     elif CNP[0] == "2":
+#         genul = "sex feminin"
+#
+# print(f"The gender based on CNP is: {genul}")
+
+CNP = "198052627169"
+print(CNP)
+print(type(CNP))
+CNP = int(CNP)
+print(type(CNP), "linia 23")

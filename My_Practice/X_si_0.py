676e93f4c589169e2d8d5efcaf2b4bb88db44a63
CasianPantilimon <casianpantilimon98@gmail.com>
2024-07-21 18:12:36 +0300

    TEMA - Validator CNP

--- My_Practice/X_si_0.py
+++ My_Practice/X_si_0.py
@@ -1,15 +1,16 @@
 import random
-from random import choice
-
 
 casute = "123456789"
 casute_cu_spatii = " ".join(casute)
 tabla_joc = casute_cu_spatii[0:5] + "\n" + casute_cu_spatii[6:11] + "\n" + casute_cu_spatii[12:]
 print(tabla_joc)
 
-for casuta in tabla_joc:
 
+
+for casuta in tabla_joc:
     casuta_user = input("- Este randul tau sa alegi o casuta: ")
+    casute = casute.replace(casuta_user, "")
+    print(casute)
     if casuta_user in tabla_joc:
         tabla_joc = tabla_joc.replace(casuta_user, "X")
         print(tabla_joc)
@@ -24,7 +25,7 @@ for casuta in tabla_joc:
             print("HUMAN WINNNNS")
             break
         # toate pozitiile castigatoare in functie de coloane
-        elif tabla_joc[0] == "X" and tabla_joc[6] == "X" and tabla_joc[11] == "X":
+        elif tabla_joc[0] == "X" and tabla_joc[6] == "X" and tabla_joc[12] == "X":
             print("HUMAN WINNNNS")
             break
         elif tabla_joc[2] == "X" and tabla_joc[8] == "X" and tabla_joc[14] == "X":
@@ -37,15 +38,19 @@ for casuta in tabla_joc:
         elif tabla_joc[0] == "X" and tabla_joc[8] == "X" and tabla_joc[16] == "X":
             print("HUMAN WINNNNS")
             break
-        elif tabla_joc[4] == "X" and tabla_joc[8] == "X" and tabla_joc[11] == "X":
+        elif tabla_joc[4] == "X" and tabla_joc[8] == "X" and tabla_joc[12] == "X":
             print("HUMAN WINNNNS")
             break
+        elif casute == "":
+            print("Remiza. Nu mai sunt mutari disponibile.")
 
     casuta_PC = random.choice(casute)
     print(f"- Computerul a ales sa joace casuta: {casuta_PC}")
+    casute = casute.replace(casuta_PC, "")
     if casuta_PC in tabla_joc:
         tabla_joc = tabla_joc.replace(casuta_PC, "0")
         print(tabla_joc)
+        print(casute)
         if tabla_joc[0] == "0" and tabla_joc[2] == "0" and tabla_joc[4] == "0":
             print("PC WINNNNS")
             break
@@ -56,7 +61,7 @@ for casuta in tabla_joc:
             print("PC WINNNNS")
             break
             # toate pozitiile castigatoare in functie de coloane pentru 0
-        elif tabla_joc[0] == "0" and tabla_joc[6] == "0" and tabla_joc[11] == "0":
+        elif tabla_joc[0] == "0" and tabla_joc[6] == "0" and tabla_joc[12] == "0":
             print("PC WINNNNS")
             break
         elif tabla_joc[2] == "0" and tabla_joc[8] == "0" and tabla_joc[14] == "0":
@@ -69,13 +74,14 @@ for casuta in tabla_joc:
         elif tabla_joc[0] == "0" and tabla_joc[8] == "0" and tabla_joc[16] == "0":
             print("PC WINNNNS")
             break
-        elif tabla_joc[4] == "0" and tabla_joc[8] == "0" and tabla_joc[11] == "0":
+        elif tabla_joc[4] == "0" and tabla_joc[8] == "0" and tabla_joc[12] == "0":
             print("PC WINNNNS")
             break
+        elif casute == "":
+            print("Remiza. Nu mai sunt mutari disponibile.")
 
 
-
-
+# pozitiile pe tabla de joc in functie de casute
 
 
 # print(tabla_joc[0], "-0")

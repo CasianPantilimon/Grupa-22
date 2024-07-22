33a910633ea415bff6f9c74f0471532a3223bdac
CasianPantilimon <casianpantilimon98@gmail.com>
2024-07-22 14:30:27 +0300

    TEMA - Validator CNP

--- TEME_Scoala_Informala/X_si_0.py
+++ TEME_Scoala_Informala/X_si_0.py
@@ -1,16 +1,39 @@
+"""
+Implementati jocul X si 0.
+
+Avem doi jucatori (X si 0) care isi vor scrie numele pe rand pe tabla de joc.
+
+Tabla are de 3 X 3 casute, si este initial goala.
+
+Jocul se incheie in momentul in care:
+
+ - pe o linie avem doar X sau doar 0
+
+ - pe o coloana avem doar X sau doar 0
+
+ - pe o diagoanala avem doar X sau doar 0
+
+- tabla s-a umplut si nu este indeplinita niciuna din conditiile de mai sus -> remiza
+
+
+"""
+
 import random
 
 casute = "123456789"
-casute_cu_spatii = " ".join(casute)
+casute_cu_spatii = " ".join(casute)  # 1 2 3 4 5 6 7 8 9
 tabla_joc = casute_cu_spatii[0:5] + "\n" + casute_cu_spatii[6:11] + "\n" + casute_cu_spatii[12:]
 print(tabla_joc)
+# 1 2 3
+# 4 5 6
+# 7 8 9
 # odata ce avem tabla de joc, putem incepe
 for casuta in tabla_joc:
     casuta_user = input("- Este randul tau sa alegi o casuta: ")
     casute = casute.replace(casuta_user, "")  # limitam optiunile user ului
     print(casute)
     if casuta_user in tabla_joc:
-        tabla_joc = tabla_joc.replace(casuta_user, "X") # inlocuim casuta aleasa cu "X"
+        tabla_joc = tabla_joc.replace(casuta_user, "X")  # inlocuim casuta aleasa cu "X"
         print(tabla_joc)
         # toate pozitiile castigatoare in functie de randuri pentru X
         if tabla_joc[0] == "X" and tabla_joc[2] == "X" and tabla_joc[4] == "X":

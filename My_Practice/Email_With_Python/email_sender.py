f84374a7b660ecafb152aef509be35ec8e40107d
CasianPantilimon <casianpantilimon98@gmail.com>
2024-07-25 10:07:49 +0300

    n/a

--- My_Practice/Email_With_Python/email_sender.py
+++ My_Practice/Email_With_Python/email_sender.py
@@ -1,9 +1,14 @@
-
 import smtplib
 
-with smtplib.SMTP_SSL('smtp.gmail.com', 465) as connection:
-    email_address = 'casianpantilimon98@gmail.com'
-    email_password = 'nisd ftan msbl xadt'
-    connection.login(email_address, email_password)
-    connection.sendmail(from_addr=email_address, to_addrs='casianpantilimon98@gmail.com',
-                        msg="subject:This is a test Email \n\n This was sent from my IDE, hehe. ")
+try:
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as email:
+        email_address = 'casianpantilimon98@gmail.com'
+        email_password = 'nisd ftan msbl xadt'
+        email.login(email_address, email_password)
+        email.sendmail(from_addr=email_address, to_addrs="casian.pantilimon98@gmail.com",
+                       msg="subject:Test Email \n\n This was sent from my IDE, hehe.\n "
+                           "Also this is sent from my IDE but, just to see if the second line gets implemented.\n\n"
+                           "Thank you,\nCasian ")
+        print("Email sent with 0 errors.")
+except (smtplib.SMTPException, smtplib.SMTPResponseException, TimeoutError, PermissionError) as err:
+    print(f"And error occured: {err}")

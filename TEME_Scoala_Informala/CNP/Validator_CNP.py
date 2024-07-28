ec2ca8765d03cf79d82d297f0087c01986cdb8cc
CasianPantilimon <casianpantilimon98@gmail.com>
2024-07-28 19:02:43 +0300

    n/a

---
+++ TEME_Scoala_Informala/CNP/Validator_CNP.py
@@ -0,0 +1,99 @@
+
+# CNP = input("Introduceti un CNP valid: ")  # cerem CNP ul sub form de input, stocat ca si "str"
+
+
+def validator_CNP(CNP):
+    if len(CNP) != 13:
+        print("CNP-ul trebuie să aibă exact 13 caractere.")  # cerem CNP ul sa aiba fix 13 caractere
+        quit()  # otherwise we will quite at once
+
+    if len(CNP) == 13:
+        control = "279146358279"
+        cifra_control = []
+        list_control = [int(i) for i in control]  # facem o lista cu caracterele din "control"
+        list_CNP = [int(i) for i in CNP]  # facem o lista cu caracterele din "CNP"
+        mixed = list(zip(list_control, list_CNP))  # le unim ca si tupluri
+        for i in mixed:
+            multiplied = i[1] * i[0]  # le inmultim
+            cifra_control.append(multiplied)
+
+        inmultite = [2, 63, 72, 0, 20, 12, 18, 10, 56, 2, 42, 81]
+        cifra_control = (sum(inmultite) % 11)  # and this will be the ALL MIGHTY C
+
+        if int(CNP[0]) not in range(1, 10):
+            return "Caracterul de pe indexul 1, nu este corect."
+        elif int(CNP[1:3]) not in range(0, 100):
+            return "Caracterele de pe indexul 2 și 3, nu sunt corecte."
+        elif int(CNP[3:5]) not in range(1, 13):
+            return "Caracterele de pe indexul 4 și 5, nu sunt corecte."
+        elif int(CNP[5:7]) not in range(1, 32):
+            return "Caracterele de pe indexul 6 și 7, nu sunt corecte."
+        elif (int(CNP[7:9]) not in range(0, 47) and int(CNP[7:9]) not in range(51, 53)) or CNP[7:9] == "00":
+            # CNP[7:9] == "00" previne user ul de a itroduce 00 pe index ul CNP[7:9]
+            return "Caracterele de pe indexul 8 și 9, nu sunt corecte."
+        elif int(CNP[9:12]) in range(0, 1000) and CNP[9:12] == "000":
+            return "Caracterele de pe indexul 10, 11 și 12 nu sunt corecte."
+        elif int(CNP[12]) != cifra_control:
+            if int(CNP[12]) != cifra_control:
+                return "Caracterul de pe indexul 13, nu este corect."
+            else:
+                return "Caracterul de pe indexul 13, este corect."
+        else:
+            # acum ca avem un CNP valid, putem sa verificam si ce inseamna numere,
+            # aflad genul, anul, luna si ziua in care s a nascut persoana
+            if CNP[0] == "1":
+                gen = "masculin, născut între 1 ianuarie 1900 și 31 decembrie 1999"
+            elif CNP[0] == "2":
+                gen = "feminin, născută între 1 ianuarie 1900 și 31 decembrie 1999"
+            elif CNP[0] == "3":
+                gen = "masculin, născut între 1 ianuarie 1800 și 31 decembrie 1899"
+            elif CNP[0] == "4":
+                gen = "feminin, născută între 1 ianuarie 1800 și 31 decembrie 1899"
+            elif CNP[0] == "5":
+                gen = "masculin, născut între 1 ianuarie 2000 și 31 decembrie 2099"
+            elif CNP[0] == "6":
+                gen = "feminin, născută între 1 ianuarie 2000 și 31 decembrie 2099"
+            elif CNP[0] == "7":
+                gen = "masculin, fiind o persoană străină rezidentă în România"
+            elif CNP[0] == "8":
+                gen = "feminin, fiind o persoană străină rezidentă în România"
+            elif CNP[0] == "9":
+                gen = "persoană străină"
+
+            an_nastere = CNP[1:3]
+
+            if CNP[3:5] == "01":
+                luna_nastere = "Ianuarie"
+            elif CNP[3:5] == "02":
+                luna_nastere = "Februarie"
+            elif CNP[3:5] == "03":
+                luna_nastere = "Martie"
+            elif CNP[3:5] == "04":
+                luna_nastere = "Aprilie"
+            elif CNP[3:5] == "05":
+                luna_nastere = "Mai"
+            elif CNP[3:5] == "06":
+                luna_nastere = "Iunie"
+            elif CNP[3:5] == "07":
+                luna_nastere = "Iulie"
+            elif CNP[3:5] == "08":
+                luna_nastere = "August"
+            elif CNP[3:5] == "09":
+                luna_nastere = "Septembrie"
+            elif CNP[3:5] == "10":
+                luna_nastere = "Octombrie"
+            elif CNP[3:5] == "11":
+                luna_nastere = "Noiembrie"
+            elif CNP[3:5] == "12":
+                luna_nastere = "Decembrie"
+
+            zi_nastere = CNP[5:7]
+
+            return (f"{CNP} este un CNP valid. "
+                    f"Persoana este de genul {gen} mai precis în anul '{an_nastere}, "
+                    f"luna {luna_nastere}, ziua {zi_nastere}.")
+
+
+print(validator_CNP("1980526271694"))
+
+
--- TEME_Scoala_Informala/X_si_0/X_si_0.py
+++ TEME_Scoala_Informala/X_si_0/X_si_0.py
@@ -20,110 +20,101 @@ Jocul se incheie in momentul in care:
 
 import random
 
-casute = "123456789"
-casute_cu_spatii = " ".join(casute)  # 1 2 3 4 5 6 7 8 9
-tabla_joc = casute_cu_spatii[0:5] + "\n" + casute_cu_spatii[6:11] + "\n" + casute_cu_spatii[12:]
-print(tabla_joc)
-# 1 2 3
-# 4 5 6
-# 7 8 9
-# odata ce avem tabla de joc, putem incepe
-for casuta in tabla_joc:
-    casuta_user = input("- Este randul tau sa alegi o casuta: ")
-    casute = casute.replace(casuta_user, "")  # limitam optiunile user ului
-    print(casute)
-    if casuta_user in tabla_joc:
-        tabla_joc = tabla_joc.replace(casuta_user, "X")  # inlocuim casuta aleasa cu "X"
-        print(tabla_joc)
-        # toate pozitiile castigatoare in functie de randuri pentru X
-        if tabla_joc[0] == "X" and tabla_joc[2] == "X" and tabla_joc[4] == "X":
-            print("HUMAN WINNNNS")
-            break
-        elif tabla_joc[6] == "X" and tabla_joc[8] == "X" and tabla_joc[10] == "X":
-            print("HUMAN WINNNNS")
-            break
-        elif tabla_joc[12] == "X" and tabla_joc[14] == "X" and tabla_joc[16] == "X":
-            print("HUMAN WINNNNS")
-            break
-        # toate pozitiile castigatoare in functie de coloane pentru X
-        elif tabla_joc[0] == "X" and tabla_joc[6] == "X" and tabla_joc[12] == "X":
-            print("HUMAN WINNNNS")
-            break
-        elif tabla_joc[2] == "X" and tabla_joc[8] == "X" and tabla_joc[14] == "X":
-            print("HUMAN WINNNNS")
-            break
-        elif tabla_joc[4] == "X" and tabla_joc[10] == "X" and tabla_joc[16] == "X":
-            print("HUMAN WINNNNS")
-            break
-        # diagonalele pentru X
-        elif tabla_joc[0] == "X" and tabla_joc[8] == "X" and tabla_joc[16] == "X":
-            print("HUMAN WINNNNS")
-            break
-        elif tabla_joc[4] == "X" and tabla_joc[8] == "X" and tabla_joc[12] == "X":
-            print("HUMAN WINNNNS")
-            break
-        elif casute == "":
-            print("Remiza. Nu mai sunt mutari disponibile.")
-            break
-    if casute != "":  # prevenim programul sa afiseze:
-        # Index Error, acest bloc ruleaza doar daca mai sunt mutari disponibile
-        casuta_PC = random.choice(casute)
-        print(f"- Computerul a ales sa joace casuta: {casuta_PC}")
-        casute = casute.replace(casuta_PC, "")  # limitam optiunile PC ului
-    else:
-        print("Remiza. Nu mai sunt mutari disponibile.")
-        break
-    if casuta_PC in tabla_joc:
-        tabla_joc = tabla_joc.replace(casuta_PC, "0")
+
+def X_0():
+    casute = "123456789"
+    casute_cu_spatii = " ".join(casute)  # 1 2 3 4 5 6 7 8 9
+    tabla_joc = casute_cu_spatii[0:5] + "\n" + casute_cu_spatii[6:11] + "\n" + casute_cu_spatii[12:]
+    print(tabla_joc)
+    # 1 2 3
+    # 4 5 6
+    # 7 8 9
+    # odata ce avem tabla de joc, putem incepe
+
+    for casuta in tabla_joc:
+        casuta_user = input("- Este randul tau sa alegi o casuta: ")
+        casute = casute.replace(casuta_user, "")  # limitam optiunile user ului
         print(casute)
-        print(tabla_joc)
-        # toate pozitiile castigatoare in functie de randuri pentru 0
-        if tabla_joc[0] == "0" and tabla_joc[2] == "0" and tabla_joc[4] == "0":
-            print("PC WINNNNS")
-            break
-        elif tabla_joc[6] == "0" and tabla_joc[8] == "0" and tabla_joc[10] == "0":
-            print("PC WINNNNS")
-            break
-        elif tabla_joc[12] == "0" and tabla_joc[14] == "0" and tabla_joc[16] == "0":
-            print("PC WINNNNS")
-            break
-        # toate pozitiile castigatoare in functie de coloane pentru 0
-        elif tabla_joc[0] == "0" and tabla_joc[6] == "0" and tabla_joc[12] == "0":
-            print("PC WINNNNS")
-            break
-        elif tabla_joc[2] == "0" and tabla_joc[8] == "0" and tabla_joc[14] == "0":
-            print("PC WINNNNS")
-            break
-        elif tabla_joc[4] == "0" and tabla_joc[10] == "0" and tabla_joc[16] == "0":
-            print("PC WINNNNS")
-            break
-            # diagonalele pentru 0
-        elif tabla_joc[0] == "0" and tabla_joc[8] == "0" and tabla_joc[16] == "0":
-            print("PC WINNNNS")
-            break
-        elif tabla_joc[4] == "0" and tabla_joc[8] == "0" and tabla_joc[12] == "0":
-            print("PC WINNNNS")
-            break
-        elif casute == "":
+        if casuta_user in tabla_joc:
+            tabla_joc = tabla_joc.replace(casuta_user, "X")  # inlocuim casuta aleasa cu "X"
+            print(tabla_joc)
+            # toate pozitiile castigatoare in functie de randuri pentru X
+            if tabla_joc[0] == "X" and tabla_joc[2] == "X" and tabla_joc[4] == "X":
+                print("HUMAN WINNNNS")
+                break
+            elif tabla_joc[6] == "X" and tabla_joc[8] == "X" and tabla_joc[10] == "X":
+                print("HUMAN WINNNNS")
+                break
+            elif tabla_joc[12] == "X" and tabla_joc[14] == "X" and tabla_joc[16] == "X":
+                print("HUMAN WINNNNS")
+                break
+            # toate pozitiile castigatoare in functie de coloane pentru X
+            elif tabla_joc[0] == "X" and tabla_joc[6] == "X" and tabla_joc[12] == "X":
+                print("HUMAN WINNNNS")
+                break
+            elif tabla_joc[2] == "X" and tabla_joc[8] == "X" and tabla_joc[14] == "X":
+                print("HUMAN WINNNNS")
+                break
+            elif tabla_joc[4] == "X" and tabla_joc[10] == "X" and tabla_joc[16] == "X":
+                print("HUMAN WINNNNS")
+                break
+            # diagonalele pentru X
+            elif tabla_joc[0] == "X" and tabla_joc[8] == "X" and tabla_joc[16] == "X":
+                print("HUMAN WINNNNS")
+                break
+            elif tabla_joc[4] == "X" and tabla_joc[8] == "X" and tabla_joc[12] == "X":
+                print("HUMAN WINNNNS")
+                break
+            elif casute == "":
+                print("Remiza. Nu mai sunt mutari disponibile.")
+                break
+        if casute != "":  # prevenim programul sa afiseze:
+            # Index Error, acest bloc ruleaza doar daca mai sunt mutari disponibile
+            casuta_PC = random.choice(casute)
+            print(f"- Computerul a ales sa joace casuta: {casuta_PC}")
+            casute = casute.replace(casuta_PC, "")  # limitam optiunile PC ului
+        else:
             print("Remiza. Nu mai sunt mutari disponibile.")
             break
+        if casuta_PC in tabla_joc:
+            tabla_joc = tabla_joc.replace(casuta_PC, "0")
+            print(casute)
+            print(tabla_joc)
+            # toate pozitiile castigatoare in functie de randuri pentru 0
+            if tabla_joc[0] == "0" and tabla_joc[2] == "0" and tabla_joc[4] == "0":
+                print("PC WINNNNS")
+                break
+            elif tabla_joc[6] == "0" and tabla_joc[8] == "0" and tabla_joc[10] == "0":
+                print("PC WINNNNS")
+                break
+            elif tabla_joc[12] == "0" and tabla_joc[14] == "0" and tabla_joc[16] == "0":
+                print("PC WINNNNS")
+                break
+            # toate pozitiile castigatoare in functie de coloane pentru 0
+            elif tabla_joc[0] == "0" and tabla_joc[6] == "0" and tabla_joc[12] == "0":
+                print("PC WINNNNS")
+                break
+            elif tabla_joc[2] == "0" and tabla_joc[8] == "0" and tabla_joc[14] == "0":
+                print("PC WINNNNS")
+                break
+            elif tabla_joc[4] == "0" and tabla_joc[10] == "0" and tabla_joc[16] == "0":
+                print("PC WINNNNS")
+                break
+                # diagonalele pentru 0
+            elif tabla_joc[0] == "0" and tabla_joc[8] == "0" and tabla_joc[16] == "0":
+                print("PC WINNNNS")
+                break
+            elif tabla_joc[4] == "0" and tabla_joc[8] == "0" and tabla_joc[12] == "0":
+                print("PC WINNNNS")
+                break
+            elif casute == "":
+                print("Remiza. Nu mai sunt mutari disponibile.")
+                break
+
+
+print(X_0())
 # pozitiile pe tabla de joc in functie de casute
 
-
-# print(tabla_joc[0], "-0")
-# print(tabla_joc[1], "-1")
-# print(tabla_joc[2], "-2")
-# print(tabla_joc[3], "-3")
-# print(tabla_joc[4], "-4")
-# print(tabla_joc[5], "-5")
-# print(tabla_joc[6], "-6")
-# print(tabla_joc[7], "-7")
-# print(tabla_joc[8], "-8")
-# print(tabla_joc[9], "-9")
-# print(tabla_joc[10], "-10")
-# print(tabla_joc[11], "-11")
-# print(tabla_joc[12], "-12")
-# print(tabla_joc[13], "-13")
-# print(tabla_joc[14], "-14")
-# print(tabla_joc[15], "-15")
-# print(tabla_joc[16], "-16")
+#
+# for idx, i in enumerate(casute_cu_spatii):
+#     print(idx,i)
